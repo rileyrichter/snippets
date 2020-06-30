662c6f33c4410e0e6dd121fdbcd0b8086455d9d5
rileyrichter <bparker@gmail.com>
2020-06-30 09:53:28 -0500

    Updated the vars to const

--- cms/cms.js
+++ cms/cms.js
@@ -1,21 +1,21 @@
 // Tweet to share template
 function tweetArticle() {
-    let author = "AUTHORPILL";
-    let article = "ARTICLENAMEPILL";
-    let slug = "SLUGPILL";
-    let fullUrl = "https://somedomain.com/post/" + slug;
-    let tweetCopy = "Hey! I just read this great article by " + author + "! Check it out here!";
-    let copyUnescaped = tweetCopy.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"').replace(/&#39;/, "'");
+    const author = "AUTHORPILL";
+    const article = "ARTICLENAMEPILL";
+    const slug = "SLUGPILL";
+    const fullUrl = "https://somedomain.com/post/" + slug;
+    const tweetCopy = `Hey! I just read this great article by + ${author}! Check it out here!`;
+    const copyUnescaped = tweetCopy.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"').replace(/&#39;/, "'");
     window.open('http://twitter.com/share?url=' + encodeURIComponent(fullUrl) + '&text=' + encodeURIComponent(copyUnescaped).replace(/'/g, "%27"))}
 
 // Rapper Radio Setup where you share an item when passing through a parameter
 function tweetRad(twitterhandle) {
-    var rrartistmaster = "ARTISTPILL";
-    var rrsongmaster = "SONGNAMEPILL";
-    var rrslugmaster = "SLUGPILL";
-    var radurl = "https://rapperradio.com/song/" + rrslugmaster;
-    var rad = "Hey, " + twitterhandle + "! I want to hear " + rrartistmaster + "'s " + rrsongmaster + " on air!";
-    var radunescaped = rad.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"').replace(/&#39;/, "'");
+    const rrartistmaster = "ARTISTPILL";
+    const rrsongmaster = "SONGNAMEPILL";
+    const rrslugmaster = "SLUGPILL";
+    const radurl = "https://rapperradio.com/song/" + rrslugmaster;
+    const rad = "Hey, " + twitterhandle + "! I want to hear " + rrartistmaster + "'s " + rrsongmaster + " on air!";
+    const radunescaped = rad.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"').replace(/&#39;/, "'");
     window.open('http://twitter.com/share?url=' + encodeURIComponent(radurl) + '&text=' + encodeURIComponent(radunescaped).replace(/'/g, "%27"))
 		$.post("https://breaksocial.herokuapp.com/req",
     {
