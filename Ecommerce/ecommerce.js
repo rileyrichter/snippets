9312730931a7468c4bb47aed5a2364d11965b75f
rileyrichter <bparker@gmail.com>
2020-06-29 20:59:03 -0500

    Added first snippet

---
+++ Ecommerce/ecommerce.js
@@ -0,0 +1,5 @@
+// Get the total of an order on the checkout page
+setTimeout(function(){
+   let myTotal = document.querySelector('.w-commerce-commercecheckoutsummarytotal').textContent;
+   console.log(myTotal);
+}, 1000);
