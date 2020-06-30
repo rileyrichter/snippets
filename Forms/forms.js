92120a84c630df0fe882025f2201ee147c76a705
rileyrichter <bparker@gmail.com>
2020-06-30 07:12:45 -0500

    Updated some code

--- Forms/forms.js
+++ Forms/forms.js
@@ -1,10 +1,8 @@
 // Refresh the page on form submit
-Webflow.push(function() {
-    $(document).on('submit', 'form', function() {
-      // Refresh page after 2000 milliseconds
-      setTimeout(function() { location.reload(true); }, 2000);
-    });
-  });
+$(document).on('submit', 'form', function() {
+  // Refresh page after 2000 milliseconds
+  setTimeout(function() { location.reload(true); }, 2000);
+});
 
   // Pass form input values through to the success state of the form
   $('#submitButtonId').click(function() {
