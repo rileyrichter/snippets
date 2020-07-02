2a703d074c557decb259f59719c311b0c9241f5c
rileyrichter <bparker@gmail.com>
2020-07-01 20:19:24 -0500

    Added a snippet to add the current selector based on ID

---
+++ css/css.js
@@ -0,0 +1,6 @@
+// get link by ID and add the current selector class
+
+setTimeout(function(){
+    let myLink = document.getElementById('on-demand');
+    myLink.className += (" w--current");
+  }, 100);  
