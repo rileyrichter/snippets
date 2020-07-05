0ecbe362e3ead5ca426bd421ce03942e53406074
rileyrichter <bparker@gmail.com>
2020-07-04 22:55:58 -0500

    Added total to local storage

--- Ecommerce/ecommerce.js
+++ Ecommerce/ecommerce.js
@@ -2,4 +2,5 @@
 setTimeout(function(){
    let myTotal = document.querySelector('.w-commerce-commercecheckoutsummarytotal').textContent;
    console.log(myTotal);
+   localStorage.setItem("Total", myTotal);
 }, 1000);
